ac4e1599753c58e8be088e57870af9599ad546ba
Steve Hopkinson <stevehopkinson@users.noreply.github.com>
2016-07-12 10:49:37 +0100

    Update historical-life-expectancy.js

--- chapter-5/historical-life-expectancy.js
+++ chapter-5/historical-life-expectancy.js
@@ -1,4 +1,5 @@
-# Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).
+/* Compute and output the average age of the people in the ancestry data set per century. A person is assigned to a century 
+by taking their year of death, dividing it by 100, and rounding it up, as in Math.ceil(person.died / 100).*/
 
 function average(array) {
   function plus(a, b) { return a + b; }
