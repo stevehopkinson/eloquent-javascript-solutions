75dbe8ee9f26085a28b42f478ffe659891986179
Steve Hopkinson <stevehopkinson@users.noreply.github.com>
2016-07-12 10:50:45 +0100

    Update every-and-then-some.js

--- chapter-5/every-and-then-some.js
+++ chapter-5/every-and-then-some.js
@@ -1,6 +1,11 @@
-# Arrays also come with the standard methods every and some. Both take a predicate function that, when called with an array element as argument, returns true or false. Just like && returns a true value only when the expressions on both sides are true, every returns true only when the predicate returns true for all elements of the array. Similarly, some returns true as soon as the predicate returns true for any of the elements. They do not process more elements than necessary—for example, if some finds that the predicate holds for the first element of the array, it will not look at the values after that.
+/* Arrays also come with the standard methods every and some. Both take a predicate function that, when called with an array 
+element as argument, returns true or false. Just like && returns a true value only when the expressions on both sides are 
+true, every returns true only when the predicate returns true for all elements of the array. Similarly, some returns true 
+as soon as the predicate returns true for any of the elements. They do not process more elements than necessary—for example, 
+if some finds that the predicate holds for the first element of the array, it will not look at the values after that.
 
-# Write two functions, every and some, that behave like these methods, except that they take the array as their first argument rather than being a method.
+Write two functions, every and some, that behave like these methods, except that they take the array as their first argument 
+rather than being a method.*/
 
 function every (array, f) {
   for (var item of array) {if (!f(item)) return false};
