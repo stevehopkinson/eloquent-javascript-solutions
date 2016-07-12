ff1921288ce872951c9da79c79d83307db39d0a9
Steve Hopkinson <stevehopkinson@users.noreply.github.com>
2016-07-12 17:48:57 +0100

    Update 4-deep-equal.js

--- chapter-4/4-deep-equal.js
+++ chapter-4/4-deep-equal.js
@@ -1,12 +1,14 @@
 /* Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects 
 with the same properties whose values are also equal when compared with a recursive call to deepEqual.*/
 
+// Does this work? Or would it just return the base case for the first property it iterates over?
+
 function deepEqual (a, b) {
 	if (typeof a === "object" && typeof b === "object") {
-        for (var property in a) {
-        	 return deepEqual(a[property], b[property]);
-        }
-    } else {
-        return a === b;
-    }	
+		for (var property in a) {
+			return deepEqual(a[property], b[property]);
+		}
+	} else {
+		return a === b;
+	}	
 }
