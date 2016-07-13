28eccdf1a17dbd554c93bbaeb5169d4626750384
Steve Hopkinson <stevehopkinson@users.noreply.github.com>
2016-07-13 23:13:38 +0100

    Update 2-fizzbuzz.js

--- chapter-2/2-fizzbuzz.js
+++ chapter-2/2-fizzbuzz.js
@@ -5,5 +5,5 @@ When you have that working, modify your program to print "FizzBuzz", for numbers
 still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
 
 for (var i = 1; i <= 100; i++) {
-  console.log(i % 15 === 0 ? "FizzBuzz" : (i % 5 === 0 ? "Buzz" : (i % 3 === 0 ? "Fizz" : i)))
+	console.log(i % 15 === 0 ? "FizzBuzz" : (i % 5 === 0 ? "Buzz" : (i % 3 === 0 ? "Fizz" : i)))
 }
